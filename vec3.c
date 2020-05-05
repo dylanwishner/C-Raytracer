779b4d482c18a6c2561820a39a48b2870a5d7514
Dylan Wishner <dylanwishner@icloud.com>
2020-05-04 20:26:04 -0700

    Vector3 operations with constants

--- vec3.c
+++ vec3.c
@@ -3,24 +3,40 @@
 //
 #include "vec3.h"
 
-Vector3 newVector(double x, double y, double z) {
+Vector3 NewVector(double x, double y, double z) {
     Vector3 vector3;
     vector3.x = x, vector3.y = y, vector3.z = z;
     return vector3;
 }
 
-Vector3 vec3_add(Vector3 a, Vector3 b) {
-    return newVector(a.x + b.x, a.y + b.y, a.z + b.z);
+Vector3 vec3Add(Vector3 a, Vector3 b) {
+    return NewVector(a.x + b.x, a.y + b.y, a.z + b.z);
 }
 
-Vector3 vec3_multiply(Vector3 a, Vector3 b) {
-    return newVector(a.x * b.x, a.y * b.y, a.z * b.z);
+Vector3 vec3Multiply(Vector3 a, Vector3 b) {
+    return NewVector(a.x * b.x, a.y * b.y, a.z * b.z);
 }
 
-Vector3 vec3_subtract(Vector3 a, Vector3 b) {
-    return newVector(a.x - b.x, a.y - b.y, a.z - b.z);
+Vector3 vec3Subtract(Vector3 a, Vector3 b) {
+    return NewVector(a.x - b.x, a.y - b.y, a.z - b.z);
 }
 
-Vector3 vec3_divide(Vector3 a, Vector3 b) {
-    return newVector(a.x / b.x, a.y / b.y, a.z / b.z);
+Vector3 vec3Divide(Vector3 a, Vector3 b) {
+    return NewVector(a.x / b.x, a.y / b.y, a.z / b.z);
+}
+
+Vector3 addConst(Vector3 vec, double f) {
+    return NewVector(vec.x + f, vec.y + f, vec.z + f);
+}
+
+Vector3 multiplyConst(Vector3 vec, double f) {
+    return NewVector(vec.x * f, vec.y * f, vec.z * f);
+}
+
+Vector3 subtractConst(Vector3 vec, double f) {
+    return NewVector(vec.x - f, vec.y - f, vec.z - f);
+}
+
+Vector3 divideConst(Vector3 vec, double f) {
+    return NewVector(vec.x / f, vec.y / f, vec.z / f);
 }
--- vec3.h
+++ vec3.h
@@ -9,4 +9,16 @@ typedef struct {
     double x, y, z;
 } Vector3;
 
+Vector3 NewVector(double x, double y, double z);
+
+Vector3 vec3Add(Vector3 a, Vector3 b);
+Vector3 vec3Multiply(Vector3 a, Vector3 b);
+Vector3 vec3Subtract(Vector3 a, Vector3 b);
+Vector3 vec3Divide(Vector3 a, Vector3 b);
+
+Vector3 addConst(Vector3 vec, double f);
+Vector3 multiplyConst(Vector3 vec, double f);
+Vector3 subtractConst(Vector3 vec, double f);
+Vector3 divideConst(Vector3 vec, double f);
+
 #endif //RAYTRACER_VEC3_H
